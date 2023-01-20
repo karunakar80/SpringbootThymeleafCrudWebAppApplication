d0bb80daf432e440acad1577487203dbe829bc7b
karunakar.medam <karunakar.medam@scriptbees.com>
2023-01-20 15:25:14 +0530

    first commit

--- src/main/java/net/javaguides/springboot/SpringbootThymeleafCrudWebAppApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootThymeleafCrudWebAppApplication.java
@@ -2,12 +2,22 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class SpringbootThymeleafCrudWebAppApplication {
+public class SpringbootThymeleafCrudWebAppApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootThymeleafCrudWebAppApplication.class, args);
 	}
 
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootThymeleafCrudWebAppApplication.class);
+	}
+	
+	
 }
